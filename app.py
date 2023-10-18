371b7e25103daa90a7a318e5088c5a9b3f99b918
cow <sokolov.csgo@gmail.com>
2023-10-18 08:03:13 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+from conf import password
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="service_db",
+    user="postgres",
+    password=12345,
+    host="localhost",
+    port="5433")
+
+cursor = conn.cursor()
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == '' or password == '':
+        return render_template('account.html', error='Login and password must be not blank')
+
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+
+    if len(records) == 0:
+        return render_template('account.html', error='Wrong login or password')
+
+    return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
